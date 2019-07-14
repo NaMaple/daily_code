fc178cfc41f8ac26f4c52b1fff7815d7d5a8aab8
yintianxiong <yintx_129@163.com>
2019-07-14 22:22:48 +0800

    swoole web_socket

--- 2019/07/13/server/web_socket.php
+++ 2019/07/13/server/web_socket.php
@@ -21,22 +21,26 @@ $ws = new swoole_websocket_server("0.0.0.0", 9502);
  */
 $ws->on('open', 'onOpen');
 function onOpen($ws, $request){
-    var_dump($request->fd, $request->get, $request->server);
+//    var_dump($request->fd, $request->get, $request->server);
     //push到客户端
-    $ws->push($request->fd, "WEB_SOCKET连接\n");
+    $ws->push($request->fd, "监听到连接事件，连接成功\n");
 }
 
 
-//监听WebSocket消息事件
+/**
+ * 监听WebSocket消息事件
+ * opcode
+ * finish
+ */
 $ws->on('message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
+    echo "接收来自客户端{$frame->fd}的消息：{$frame->data}，opcode为：{$frame->opcode}，fin为：{$frame->finish}\n";
     //push到客户端
-    $ws->push($frame->fd, "server: {$frame->data}");
+    $ws->push($frame->fd, "监听到消息事件，消息内容是: {$frame->data}");
 });
 
 //监听WebSocket连接关闭事件
 $ws->on('close', function ($ws, $fd) {
-    echo "client-{$fd} is closed\n";
+    echo "客户端ID-{$fd}被关闭\n";
 });
 
 $ws->start();
