66cef38c4855520b3ac6144c920388429b3d75d4
yintianxiong <yintx_129@163.com>
2019-07-17 17:29:53 +0800

    phpspider demo xpath

--- 2019/07/15/index.php
+++ 2019/07/15/index.php
@@ -7,41 +7,69 @@
  */
 
 require './vendor/autoload.php';
+
+
 use phpspider\core\phpspider;
+use phpspider\core\requests;    //请求类
+use phpspider\core\selector;    //选择器类
+use phpspider\core\db;          //选择器类
+use phpspider\core\log;         //选择器类
 
 /* Do NOT delete this comment */
 /* 不要删除这段注释 */
 
 $configs = array(
-    'name' => '糗事百科',
+    'name' => '头条财经',
     'domains' => array(
-        'qiushibaike.com',
-        'www.qiushibaike.com'
+        'www.toutiao.com'
     ),
     'scan_urls' => array(
-        'http://www.qiushibaike.com/'
+        'https://www.toutiao.com/ch/investment/',
+//        'https://www.toutiao.com/ch/stock_channel/',
+//        'https://www.toutiao.com/ch/finance_management/',
+//        'https://www.toutiao.com/ch/macro_economic/',
     ),
     'content_url_regexes' => array(
-        "http://www.qiushibaike.com/article/\d+"
+//        "https://www.toutiao.com/group/\d+"
     ),
     'list_url_regexes' => array(
-        "http://www.qiushibaike.com/8hr/page/\d+\?s=\d+"
+//        "http://www.qiushibaike.com/8hr/page/\d+\?s=\d+"
+    ),
+    'log_show' => true,
+    //日志存放的位置
+    'log_file' => './data/phpspider.log',
+    'export' => array(
+        'type' => 'csv',
+        'file' => './data/phpspider.csv', //爬下来的数据放在data目录下，目录和文件要自己提前创建好
+    ),
+    'log_type' => 'error,debug,warn,error',
+    //爬虫爬取网页所使用的浏览器类型.随机浏览器类型，用于破解防采集
+    'user_agent' => array(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13G34 Safari/601.1",
+        "Mozilla/5.0 (Linux; U; Android 6.0.1;zh_cn; Le X820 Build/FEXCNFN5801507014S) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 Chrome/49.0.0.0 Mobile Safari/537.36 EUI Browser/5.8.015S",
     ),
     'fields' => array(
+//        array(
+//            // 抽取内容页列表
+//            'name' => "article_content",
+//            'selector' => "//*[@id='single-next-link']",
+//            'required' => true
+//        ),
         array(
-            // 抽取内容页的文章内容
-            'name' => "article_content",
-            'selector' => "//*[@id='single-next-link']",
-            'required' => true
-        ),
-        array(
-            // 抽取内容页的文章作者
-            'name' => "article_author",
-            'selector' => "//div[contains(@class,'author')]//h2",
-            'required' => true
+            'name' => "article_title",
+//            'selector' => "//div[contains(@class,'wcommonFeed')]//ul//li[contains(@class,'item    ')]",
+//            'selector' => "//div[contains(@ga_event,'article_title_click')]",
+            'selector' => "//a[contains(@class,'link title')]",
+            'required' => true,
+            'repeated' => true
         ),
     ),
 );
 $spider = new phpspider($configs);
 $spider->start();
+
+$spider->on_extract_field = function($fieldname, $data, $page){
+    var_dump($data);
+};
 
