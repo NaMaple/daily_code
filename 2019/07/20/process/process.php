270ce029778bf2505280e14f1001b9cc1fc9b6e9
yintianxiong <yintx_129@163.com>
2019-07-21 18:51:42 +0800

    进程

--- 2019/07/20/process/process.php
+++ 2019/07/20/process/process.php
@@ -14,8 +14,13 @@ $process = new swoole_process(function(swoole_process $process){
     //在子进程中启用一个HTTP服务
     //在Linux中执行PHP
     //__DIR__ 当前文件所在目录，到文件夹
-    $process->exec('/usr/local/opt/php@7.1/bin/php', [__DIR__.'/../13/server/http.php']);
-}, true);
+
+    $phpPath = '/usr/local/opt/php@7.1/bin/php';
+    $httpPath = __DIR__.'/../13/server/http.php';
+    //HTTP启动不来
+    $process->exec($phpPath, [$httpPath]);
+    echo $httpPath . PHP_EOL;
+}, false);
 
 //启用一个子进程
 $pid = $process->start();
