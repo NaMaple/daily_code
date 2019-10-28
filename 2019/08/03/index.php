fb7aa43cec738190dd89af86691f5a3d9767387d
yintianxiong <yintx_129@163.com>
2019-10-28 21:25:42 +0800

    测试NGINX配置

--- 2019/08/03/index.php
+++ 2019/08/03/index.php
@@ -1,12 +1,2 @@
 <?php
     echo 1;die;
-
-    function muMu($i) {
-        $times = $i++;
-        if ($i >= 1) {
-            return '沐沐小可爱，我想你了/n';
-            muMu($times);
-        }
-    }
-
-    echo muMu(1);
