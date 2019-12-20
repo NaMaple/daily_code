8f920482a171f6301b24bb40c9f24f33c2cfc0e3
yintianxiong <yintx_129@163.com>
2019-12-21 00:13:48 +0800

    swoole，thinkphp和sw整合

--- thinkphp/server/http_server.php
+++ thinkphp/server/http_server.php
@@ -29,25 +29,70 @@ $http->set(
  */
 $http->on('WorkerStart', function(swoole_server $server, $work_id) {
     // 加载框架引导文件
-//    require __DIR__ . '/../thinkphp/base.php';
+    require __DIR__ . '/../thinkphp/base.php';
 
-    require __DIR__ . '/../thinkphp/start.php';
+    /**
+     * Container::get('app', [defined('APP_PATH') ? APP_PATH : ''])->run()->send();
+     * start执行控制器代码，worker进程里没有必要执行。只有热加载一些文件即可
+     * 在request里面执行处理http请求的逻辑
+     */
+//    require __DIR__ . '/../thinkphp/start.php';
 });
 
 $http->on('request', function($request, $response) {
-    $content = [
-        'data:' => NOW_TIME,
-        'get:' => $request->get,
-        'post:' => $request->post,
-        'header:' => $request->header,
-    ];
+//    $content = [
+//        'data:' => NOW_TIME,
+//        'get:' => $request->get,
+//        'post:' => $request->post,
+//        'header:' => $request->header,
+//    ];
 
-    $response->cookie("cookie_key", "cookie_value", time() + 1800);
-    $response->end("end:" . json_encode($request->get));
+    if(isset($request->server)) {
+        foreach ($request->server as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->header)) {
+        foreach ($request->header as $k=>$v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+
+    if(isset($request->get)) {
+        foreach ($request->get as $k=>$v) {
+            $_GET[$k] = $v;
+        }
+    }
+
+    if(isset($request->post)) {
+        foreach ($request->post as $k=>$v) {
+            $_POST[$k] = $v;
+        }
+    }
+
+//    var_dump($request);
+
+    ob_start();
+    try {
+        think\Container::get('app', [defined('APP_PATH') ? APP_PATH : ''])->run()->send();
+    } catch (\Exception $e) {
+        //todo 输出业务逻辑
+    }
+
+    $res = ob_get_contents();
+    var_dump($res);
+    ob_clean();
+    $response->end($res);
+//    $response->cookie("cookie_key", "cookie_value", time() + 1800);
+
+
+
+//    $response->end("end:" . json_encode($request->get));
 });
 
 /**
  * 开启http_server php http_server.php
- * 关闭http_server
+ * 关闭http_server control+c
  */
 $http->start();
