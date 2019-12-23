5f80237306270fccd7d834651c3bfe2695b1d4fe
yintianxiong <yintx_129@163.com>
2019-12-24 01:40:16 +0800

    swoole，thinkphp和sw整合

--- thinkphp/application/index/controller/Index.php
+++ thinkphp/application/index/controller/Index.php
@@ -17,9 +17,13 @@ class Index
 
     public function hello($name = 'ThinkPHP5')
     {
+        /**
+         * hellozhang
+         * todo index()中name=zhang，访问了index再访问hello，name变成了zhang?
+         */
         var_dump($_GET);
         var_dump('hello');
-        return 'hello' . $name . time();
+        return 'hello ' . $name . time() . PHP_EOL;
     }
 
 }
