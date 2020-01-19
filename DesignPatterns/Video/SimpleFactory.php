0ba296e032c4f059d8e8a51ba1cf292c8881b761
yintianxiong <yintx_129@163.com>
2020-01-19 17:36:00 +0800

    工厂方法模式

--- DesignPatterns/Video/SimpleFactory.php
+++ DesignPatterns/Video/SimpleFactory.php
@@ -1,59 +1,59 @@
 <?php
-
-/**
- * 服务端打包开始
- * 面向接口开发，规范开发，共同接口
- */
-interface db {
-    function connect();
-}
-
-/**
- * 服务端开发（不知道被谁调用）
- * 只要实现接口即可，有点拆分项目的意思
- */
-class dbMysql implements db{
-    public function connect()
-    {
-        echo '连接上MySQL数据库' . PHP_EOL;
-    }
-}
-
-class dbSqlite implements db{
-    public function connect()
-    {
-        echo '连接上Sqlite数据库' . PHP_EOL;
-    }
-}
-
-
-
-/**
- * 简单工厂模式
- */
-class Factory {
-    public static function createDB($type) {
-        if ($type == 'mysql') {
-            return new dbMysql();
-        } elseif ($type == 'sqlite') {
-            return new dbSqlite();
-        } else {
-            throw new Exception('Error DB Type', 1);
-        }
-    }
-}
-
-/**
- * 客户端不知道服务器端到底有哪些类，把类名隐藏了
- * 只知道对方开发了一个Factory::createDB方法
- * 方法只允许传递数据库名称
- *
- * 新增oracle类型，服务端需要修改Factory内容
- * 在OOD（面向对象思想）中有一个开闭原则，对于修改是封闭的，对于扩展是开放的。
- * 换一个子类实现
- */
-
-$mysql = Factory::createDB('mysql');
-$mysql->connect();
-$sqlite = Factory::createDB('sqlite');
-$sqlite->connect();
+//
+///**
+// * 服务端打包开始
+// * 面向接口开发，规范开发，共同接口
+// */
+//interface db {
+//    function connect();
+//}
+//
+///**
+// * 服务端开发（不知道被谁调用）
+// * 只要实现接口即可，有点拆分项目的意思
+// */
+//class dbMysql implements db{
+//    public function connect()
+//    {
+//        echo '连接上MySQL数据库' . PHP_EOL;
+//    }
+//}
+//
+//class dbSqlite implements db{
+//    public function connect()
+//    {
+//        echo '连接上Sqlite数据库' . PHP_EOL;
+//    }
+//}
+//
+//
+//
+///**
+// * 简单工厂模式
+// */
+//class Factory {
+//    public static function createDB($type) {
+//        if ($type == 'mysql') {
+//            return new dbMysql();
+//        } elseif ($type == 'sqlite') {
+//            return new dbSqlite();
+//        } else {
+//            throw new Exception('Error DB Type', 1);
+//        }
+//    }
+//}
+//
+///**
+// * 客户端不知道服务器端到底有哪些类，把类名隐藏了
+// * 只知道对方开发了一个Factory::createDB方法
+// * 方法只允许传递数据库名称
+// *
+// * 新增oracle类型，服务端需要修改Factory内容
+// * 在OOD（面向对象思想）中有一个开闭原则，对于修改是封闭的，对于扩展是开放的。
+// * 换一个子类实现
+// */
+//
+//$mysql = Factory::createDB('mysql');
+//$mysql->connect();
+//$sqlite = Factory::createDB('sqlite');
+//$sqlite->connect();
