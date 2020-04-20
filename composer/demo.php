55db1aab1bc4e66af744bdac77824bac9e77c2e5
yintianxiong <yintx_129@163.com>
2020-04-20 23:33:45 +0800

    composer

---
+++ composer/demo.php
@@ -0,0 +1,15 @@
+<?php
+
+# 自动加载
+require "vendor/autoload.php";
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('Redis');
+$log->pushHandler(new StreamHandler(__DIR__ . '.redis.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('redis','ip err');
+$log->error('Bar');
